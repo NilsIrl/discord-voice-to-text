8a03d74915fb9a76331468cb728c20f94f94f38e
Nils <nils@nilsand.re>
2023-04-18 20:47:16 +0100

    Respond to all messages in DM

--- bot.py
+++ bot.py
@@ -8,12 +8,14 @@ ATTACHMENT_DIR = Path(tempfile.mkdtemp())
 
 DEFAULT_MODEL = "small"
 MODELS = {
-        "small": whisper.load_model("small"),
-        }
+    "small": whisper.load_model("small"),
+}
+
 
 def get_text(filename):
     result = MODELS[DEFAULT_MODEL].transcribe(str(filename))
     return result["text"]
+
 
 async def attachment_to_text(attachment: discord.Attachment):
     filename = ATTACHMENT_DIR / str(attachment.id)
@@ -21,6 +23,7 @@ async def attachment_to_text(attachment: discord.Attachment):
     text = get_text(filename)
     os.remove(filename)
     return text
+
 
 class Voice2Text(discord.Client):
     def __init__(self, *, intents: discord.Intents):
@@ -33,16 +36,22 @@ class Voice2Text(discord.Client):
 
         # flag for voice message is (1 << 13)
         # it is not yet documented in https://discord.com/developers/docs/resources/channel#message-object-message-flags
-        if message.flags.value & (1 << 13):
+        if (
+            message.flags.value & (1 << 13)
+            or isinstance(message.channel, discord.DMChannel)
+            and len(message.attachments) > 0
+        ):
             assert len(message.attachments) == 1
             await message.reply(await attachment_to_text(message.attachments[0]))
 
     async def setup_hook(self):
         await self.tree.sync()
 
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = Voice2Text(intents=intents)
+
 
 @bot.tree.context_menu(name="Voice to text")
 async def voice_to_text(interaction: discord.Interaction, message: discord.Message):
@@ -50,5 +59,6 @@ async def voice_to_text(interaction: discord.Interaction, message: discord.Messa
     for attachment in message.attachments:
         await interaction.followup.send(await attachment_to_text(attachment))
 
-bot.run(os.environ['DISCORD_TOKEN'])
+
+bot.run(os.environ["DISCORD_TOKEN"])
 os.removedirs(ATTACHMENT_DIR)
